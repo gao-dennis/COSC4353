1c34833a1b875510872908c48025bc4a13737110
Dennis Gao <59455856+gao-dennis@users.noreply.github.com>
2022-03-13 19:39:18 -0500

    Add files via upload

---
+++ PricingModule.py
@@ -0,0 +1,8 @@
+class PricingModule:
+  def __init__(self, currentPrice, locationFactor, rateHistoryFactor, gallonsRequested, companyProfit):
+    self.currentPrice = currentPrice
+    self.locationFactor = locationFactor
+    self.rateHistoryFactor = rateHistoryFactor
+    self.gallonsRequested = gallonsRequested
+    self.companyProfit = companyProfit
+
